c87eaa38f78543a6129adaf90c26dd6abdcce082
Vivek Gautam <vivekbgautam@gmail.com>
2025-12-01 12:50:44 +0530

    Add files via upload

---
+++ Assignment/Assignment_39/program39_5.cpp
@@ -0,0 +1,54 @@
+// recursive program which Display below pattern 
+
+// 1    2   3   4   5
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Required Header File
+//
+////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Function Name   :   Display
+//  Description     :   It is A recursive function which call itself 
+//                      And Display Desired pattern 
+//  Input           :   Integer
+//  Output          :   Integer
+//  Author          :   Vivek Bhauraj Gautam
+//  Date            :   01/12/2025
+//
+////////////////////////////////////////////////////////////////////////
+
+void Display(int iNo)
+{
+    static int cValue = 0;
+    static char Ch = 'a';
+
+    if(cValue < iNo )
+    {
+        cout<<Ch<<"\t";
+        cValue++;
+        Ch++;
+        Display(iNo);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function : Main
+//
+////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue = 0;
+    cout<<"Enter Value :\n";
+    cin>>iValue;
+    Display(iValue);
+
+    return 0;
+}
